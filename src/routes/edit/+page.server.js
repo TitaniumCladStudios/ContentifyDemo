31a3002f816987e33661c27175b2e9fe426bd67b
rboddy <avidryanb@gmail.com>
2023-05-08 22:38:47 -0400

    Now we're cooking with gas baby!

--- src/routes/edit/+page.server.js
+++ src/routes/edit/+page.server.js
@@ -1,4 +1,20 @@
+import { parse } from "node-html-parser";
+
 export async function load(){
 
-    console.log("Hello!");
+    let page = await fetch("http://localhost:5173/")
+    .then(response => response.text())
+    .then(html => {
+        return html;
+    })
+
+    const root = parse(page)
+
+    let editable = root.querySelectorAll("[data-contentify]")
+
+    for(let element of editable){
+        let contentify = element.getAttribute("data-contentify");
+        console.log(contentify);
+        console.log(element.innerText);
+    }
 }
