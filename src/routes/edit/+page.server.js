fb9cb9f98a707dcdb31ddc1a535ed307b7634c3d
rboddy <avidryanb@gmail.com>
2023-05-11 23:36:43 -0400

    Getting closer?

--- src/routes/edit/+page.server.js
+++ src/routes/edit/+page.server.js
@@ -2,25 +2,26 @@ import { parse } from "node-html-parser";
 import { createClient } from 'redis';
 import { env } from "$env/dynamic/private"
 import { redirect } from '@sveltejs/kit';
-
+import { getAuth } from "firebase/auth";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDo1bBJXVQS7EQZtMc0xXlj-UGTEssrhFE",
-  authDomain: "contentifydemo.firebaseapp.com",
-  projectId: "contentifydemo",
-  storageBucket: "contentifydemo.appspot.com",
-  messagingSenderId: "829223919941",
-  appId: "1:829223919941:web:67515f228008e674031e2c"
+    apiKey: "AIzaSyDo1bBJXVQS7EQZtMc0xXlj-UGTEssrhFE",
+    authDomain: "contentifydemo.firebaseapp.com",
+    projectId: "contentifydemo",
+    storageBucket: "contentifydemo.appspot.com",
+    messagingSenderId: "829223919941",
+    appId: "1:829223919941:web:67515f228008e674031e2c"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 
 const client = createClient({
     url: "redis://default:XoHxlMoZc52Ep17ubr4mWHioIgmflR48@redis-14041.c8.us-east-1-4.ec2.cloud.redislabs.com:14041"
@@ -30,6 +31,8 @@ export async function load(){
 
     // Initialize Firebase Authentication and get a reference to the service
     const auth = getAuth(app);
+
+    console.log(auth.currentUser);
 
     if(auth.currentUser == null){
         throw redirect(307, '/login');
--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -3,6 +3,22 @@ import { z } from 'zod';
 import { superValidate } from 'sveltekit-superforms/server';
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { redirect } from '@sveltejs/kit';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDo1bBJXVQS7EQZtMc0xXlj-UGTEssrhFE",
+    authDomain: "contentifydemo.firebaseapp.com",
+    projectId: "contentifydemo",
+    storageBucket: "contentifydemo.appspot.com",
+    messagingSenderId: "829223919941",
+    appId: "1:829223919941:web:67515f228008e674031e2c"
+};
+
 
 const schema = z.object({
   email: z.string().email(),
@@ -31,6 +47,7 @@ export const actions = {
     signInWithEmailAndPassword(auth, form.data.email, form.data.password)
     .then((userCredential) => {
         // Signed in 
+        console.log("Signed in");
         const user = userCredential.user;
         throw redirect(307, '/edit');
     })
