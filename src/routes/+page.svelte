c35a42214d7b08ccba052f21a233451be9e2f62e
rboddy <avidryanb@gmail.com>
2023-05-11 22:33:48 -0400

    Adding a subtle box shadow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,6 +36,7 @@
         padding: 30px;
         background-color: #fff;
         border-radius: 30px;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     }
     a {
         color: #e35656;
