c40728d917449b0db9582aaa3e67925ba06cc406
rboddy <avidryanb@gmail.com>
2023-05-11 22:52:52 -0400

    Added a login

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+<div class="content-container">
+    <h1>Login to Edit</h1>
+</div>
+
+<style>
+    .content-container {
+        width: 40%;
+        padding: 30px;
+        background-color: #fff;
+        border-radius: 30px;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    }
+    @media(max-width: 850px){
+        .content-container {
+            width: 90%;
+            margin: 10px;
+        }
+    }
+</style>
